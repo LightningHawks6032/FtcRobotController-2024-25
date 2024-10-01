9d41579ee3b0979155640de4011263d0e4e948d8
Carmel Barkaee <carmelbarkaee032708@gmail.com>
2024-10-01 14:41:29 -0500

    Added Motor Controls

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestingTeleop.java
@@ -4,9 +4,105 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 @TeleOp(name = "Testing")
 public class TestingTeleop extends OpMode {
+    //Data class that gets passed into a process
+    public static class ProcessData {
+
+    }
+    //Defines a process to run on initialization and every loop call
+    public interface IProcess {
+        void loop(ProcessData data);
+        void init(ProcessData data);
+    }
+    //Exposes motor functionality
+    public static final class MotorControls implements IProcess{
+        DcMotor ul, ur, ll, lr; // up left, up right, low left, low right
+        Gamepad gamepad;
+        void powerAllMotors(double power) {
+            ul.setPower(power);
+            ur.setPower(power);
+            ll.setPower(power);
+            lr.setPower(power);
+        }
+
+        //positive power -> forward, negative power -> backward
+        void moveForward() {
+            ul.setDirection(DcMotor.Direction.FORWARD);
+            ur.setDirection(DcMotor.Direction.FORWARD);
+            ll.setDirection(DcMotor.Direction.FORWARD);
+            lr.setDirection(DcMotor.Direction.FORWARD);
+        }
+        //positive power -> right, negative power -> backward
+        void moveSide(double power) {
+            if (power == 0) {return;}
+            if (power > 0) {
+                ul.setDirection(DcMotor.Direction.FORWARD);
+                ur.setDirection(DcMotor.Direction.REVERSE);
+                ll.setDirection(DcMotor.Direction.REVERSE);
+                lr.setDirection(DcMotor.Direction.FORWARD);
+            }
+            else {
+                ul.setDirection(DcMotor.Direction.REVERSE);
+                ur.setDirection(DcMotor.Direction.FORWARD);
+                ll.setDirection(DcMotor.Direction.FORWARD);
+                lr.setDirection(DcMotor.Direction.REVERSE);
+            }
+        }
+
+        //positive power -> clockwise, negative power -> counter clockwise
+        void rotate(double power) {
+            if (power == 0) {return;}
+            if (power > 0) {
+                ul.setDirection(DcMotor.Direction.FORWARD);
+                ur.setDirection(DcMotor.Direction.REVERSE);
+                ll.setDirection(DcMotor.Direction.FORWARD);
+                lr.setDirection(DcMotor.Direction.REVERSE);
+            }
+            else {
+                ul.setDirection(DcMotor.Direction.REVERSE);
+                ur.setDirection(DcMotor.Direction.FORWARD);
+                ll.setDirection(DcMotor.Direction.REVERSE);
+                lr.setDirection(DcMotor.Direction.FORWARD);
+            }
+        }
+
+        public MotorControls(DcMotor _ul, DcMotor _ur, DcMotor _ll, DcMotor _lr, Gamepad _gamepad) {
+            ul = _ul;
+            ur = _ur;
+            ll = _ll;
+            lr = _lr;
+            gamepad = _gamepad;
+        }
+
+        @Override
+        public void loop(ProcessData data) {
+            float yaxis = gamepad.left_stick_y;
+            float xaxis = gamepad.left_stick_x;
+            float raxis = gamepad.right_stick_x;
+
+            if (yaxis != 0 ) {
+                moveForward();
+                powerAllMotors(yaxis * 0.5);
+            }
+            else if (xaxis != 0) {
+                moveSide(xaxis);
+                powerAllMotors(xaxis);
+            }
+            else if (raxis != 0) {
+                rotate(raxis);
+                powerAllMotors(raxis);
+            }
+
+        }
+
+        @Override
+        public void init(ProcessData data) {
+
+        }
+    }
 
     DcMotor motorupleft;
     DcMotor motorupright;
@@ -15,7 +111,7 @@ public class TestingTeleop extends OpMode {
     //DcMotor slideleft;
     //DcMotor slideright;
 
-
+    MotorControls m;
     @Override
     public void init() {
 
@@ -31,15 +127,15 @@ public class TestingTeleop extends OpMode {
         motorlowleft.setDirection(DcMotor.Direction.FORWARD);
         motorupright.setDirection(DcMotor.Direction.REVERSE);
         motorlowright.setDirection(DcMotor.Direction.REVERSE);
+        m = new MotorControls(motorupleft, motorupright, motorlowleft, motorlowright, gamepad1);
         //slideleft.setDirection(DcMotor.Direction.FORWARD);
-
-
     }
 
     @Override
     public void loop() {
+        m.loop(new ProcessData());
 
-        motorupleft.setPower(gamepad1.left_stick_y);
+        /*motorupleft.setPower(gamepad1.left_stick_y);
         motorupright.setPower(gamepad1.right_stick_y);
         motorlowleft.setPower(gamepad1.left_stick_y);
         motorlowright.setPower(gamepad1.right_stick_y);
@@ -58,12 +154,6 @@ public class TestingTeleop extends OpMode {
             motorlowright.setDirection(DcMotor.Direction.FORWARD);
 
         }
-        else {
-            motorupleft.setPower(0);
-            motorupright.setPower(0);
-            motorlowleft.setPower(0);
-            motorlowleft.setPower(0);
-        }
         if(gamepad1.left_bumper)
         {
             motorupleft.setPower(0.5);
@@ -75,14 +165,7 @@ public class TestingTeleop extends OpMode {
             motorupright.setDirection(DcMotor.Direction.FORWARD);
             motorlowright.setDirection(DcMotor.Direction.REVERSE);
 
-        }
-        else
-        {
-            motorupleft.setPower(0);
-            motorupright.setPower(0);
-            motorlowleft.setPower(0);
-            motorlowleft.setPower(0);
-        }
-        }
-
+        }*/
     }
+
+}
